264b3a6d1e5920fa2476c77709e6c48b8a3399da
Aditya <adipusk@gmail.com>
2024-02-21 13:10:27 +0530

    Add helper functions for data data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -4,8 +4,6 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
-from src.utils import save_object
-
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
@@ -14,6 +12,9 @@ from sklearn.preprocessing import LabelEncoder
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
+from src.utils import DataPrep
+from src.utils import Encoding
 
 
 @dataclass
@@ -26,7 +27,7 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         try:
-            numerical_features = ['Year', 
+            features = ['Year', 
                                   'Total Score for Team 1', 
                                   'Total Wicket for Team 1', 
                                   'Total Score for Team 2', 
@@ -35,42 +36,30 @@ class DataTransformation:
                                   'Runs Scored in over', 
                                   'Runs Scored till that over', 
                                   'Wicket Taken in over', 
-                                  'Wickets Taken till that over']
+                                  'Wickets Taken till that over',
+                                  'Match Number', 
+                                  'City', 
+                                  'Date', 
+                                  'Winner', 
+                                  'Team 1', 
+                                  'Team 2', 
+                                  'Batting team', 
+                                  'Bowling team', 
+                                  'Winning Details', 
+                                  'Match Detail']
             
-            categorical_features = ['Match Number', 
-                                    'City', 
-                                    'Date', 
-                                    'Winner', 
-                                    'Team 1', 
-                                    'Team 2', 
-                                    'Batting team', 
-                                    'Bowling team', 
-                                    'Winning Details', 
-                                    'Match Detail']
-            
-            num_pipeline = Pipeline(
+            pipeline = Pipeline(
                 steps=[
-                    ('imputer',SimpleImputer(strategy='median')),
-                    ('scaler',StandardScaler())
+                    ('prep',DataPrep()),
+                    ('encoder',Encoding())
                 ]
             )
 
-            logging.info(f'Numerical columns in pipeline : {numerical_features}')
-
-            cat_pipeline=Pipeline(
-                steps=[
-                    ('imputer',SimpleImputer(strategy='most_frequent')),
-                    ('encoder',LabelEncoder())
-                ]
-            )
-
-            
-            logging.info(f'Categorical columns in pipeline : {categorical_features}')
+            logging.info(f'Columns in pipeline : {features}')
 
             preprocessor = ColumnTransformer(
                 [
-                    ('num_pipe', num_pipeline, numerical_features),
-                    ('cat_pipe', cat_pipeline, categorical_features)
+                    ('pipe', pipeline, features),
                 ]
             )
 
@@ -92,22 +81,35 @@ class DataTransformation:
 
             preprocessing_obj = self.get_data_transformer_object()
 
-            target_column_name = ''
-            numerical_columns = ['']
+            inp_train_arr = pd.DataFrame(preprocessing_obj.fit_transform(train_df))
+            inp_test_arr = pd.DataFrame(preprocessing_obj.fit_transform(test_df))
+            # print(inp_train_arr)
 
-            inp_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df = train_df[target_column_name]
 
-            inp_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df = test_df[target_column_name]
+            target_column_name = 3
+            # Columns = ['Over Number', 'Runs Scored till that over',
+            #            'Wickets Taken till that over', 'Total Runs', 'Runs in Last 5 Overs',
+            #            'Wickets in Last 5 Overs', 'Batting team AFG', 'Batting team AUS',
+            #            'Batting team BAN', 'Batting team ENG', 'Batting team IND', 'Batting team IRE',
+            #            'Batting team NED', 'Batting team NZ', 'Batting team PAK', 'Batting team SA',
+            #            'Batting team SL', 'Batting team WI', 'Bowling team AFG',
+            #            'Bowling team AUS', 'Bowling team BAN', 'Bowling team ENG', 'Bowling team IND',
+            #            'Bowling team IRE', 'Bowling team NED', 'Bowling team NZ', 'Bowling team PAK',
+            #            'Bowling team SA', 'Bowling team SL', 'Bowling team WI']
+
+            inp_feature_train_df = inp_train_arr.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df = inp_train_arr[target_column_name]
+
+            inp_feature_test_df = inp_test_arr.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df = inp_test_arr[target_column_name]
 
             logging.info('Applying preprocessing object on training and testing dataframe.')
 
-            inp_train_arr = preprocessing_obj.fit_transform(inp_feature_train_df)
-            inp_test_arr = preprocessing_obj.fit_transform(inp_feature_test_df)
+            
 
-            train_arr = np.c_[inp_train_arr, np.array(target_feature_train_df)]
-            test_arr = np.c_[inp_test_arr, np.array(target_feature_test_df)]
+
+            train_arr = np.c_[inp_feature_train_df, np.array(target_feature_train_df)]
+            test_arr = np.c_[inp_feature_test_df, np.array(target_feature_test_df)]
 
             save_object(
                 file_path=self.data_transformation_config.preprocessor_ob_path,
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,9 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+pd.options.mode.chained_assignment = None
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelEncoder
 
 
 from src.exception import CustomException
@@ -18,3 +21,68 @@ def save_object(file_path, obj):
         
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+class DataPrep(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self,df, y=None):
+        df['Total Runs'] = None
+        df['Total Wickets'] = None
+
+        for i in range(len(df)):
+            if(df['Team 1'][i] == df['Batting team'][i]):
+                df['Total Runs'][i] = df['Total Score for Team 1'][i]
+                df['Total Wickets'][i] = df['Total Wicket for Team 1'][i]
+            else:
+                df['Total Runs'][i] = df['Total Score for Team 2'][i]
+                df['Total Wickets'][i] = df['Total Wicket for Team 2'][i]
+
+        df['Total Runs'] = df['Total Runs'].astype(int)
+
+        df['Runs in Last 5 Overs'] = df['Runs Scored till that over'].diff(periods=5).fillna(0)
+        df['Wickets in Last 5 Overs'] = df['Wickets Taken till that over'].diff(periods=5).fillna(0)
+
+        df.drop(['Team 1', 'Team 2', 'Total Score for Team 1', 'Total Score for Team 2', 'Total Wicket for Team 2', 'Total Wicket for Team 1', 'City','Year', 'Match Number', 'Date', 'Winner','Winning Details', 'Match Detail', 'Total Wickets', 'Runs Scored in over', 'Wicket Taken in over'], axis = 1, inplace = True)
+
+        popular_teams = ['ENG', 'NZ', 'PAK', 'NED', 'AFG', 'BAN', 'SA', 'SL', 'AUS', 'IND',
+        'WI', 'IRE']
+        
+        df = df[(df['Batting team'].isin(popular_teams)) & (df['Bowling team'].isin(popular_teams))]
+
+        return df
+    
+
+
+class Encoding(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
+        pass
+
+    def fit(self,X,y=None):
+        return self
+    
+    def transform(self,df, y=None):
+        L = LabelEncoder()
+        for col in ['Batting team', 'Bowling team']:
+            df[col] = L.fit_transform(df[col])
+
+        columns =  ['Over Number', 'Runs Scored till that over',
+        'Wickets Taken till that over', 'Total Runs', 'Runs in Last 5 Overs',
+        'Wickets in Last 5 Overs', 'Batting team AFG', 'Batting team AUS',
+        'Batting team BAN', 'Batting team ENG', 'Batting team IND', 'Batting team IRE',
+        'Batting team NED', 'Batting team NZ', 'Batting team PAK', 'Batting team SA',
+        'Batting team SL', 'Batting team WI', 'Bowling team AFG',
+        'Bowling team AUS', 'Bowling team BAN', 'Bowling team ENG', 'Bowling team IND',
+        'Bowling team IRE', 'Bowling team NED', 'Bowling team NZ', 'Bowling team PAK',
+        'Bowling team SA', 'Bowling team SL', 'Bowling team WI']
+        
+        df_encoded = pd.get_dummies(df, columns=['Batting team', 'Bowling team'])
+
+        df_encoded.columns = columns
+
+        return df_encoded
+    
